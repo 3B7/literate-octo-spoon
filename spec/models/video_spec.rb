16f5e578ba103b9d29dfbe614b0f2d4df41fe486
erik batres <erikbatres@gmail.com>
2016-02-01 14:31:27 -0800

    fix video_spec.rb

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe User do
+describe Video do
   let(:video) { FactoryGirl.create(:video) }
   it 'should respond to users' do
     expect(video).to respond_to(:users)
