34762da42aa765cc5957c20e592027d603d1513d
erik batres <erikbatres@gmail.com>
2016-02-01 12:24:08 -0800

    redis tweaks

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
-  $redis = Redis.new(ENV['REDISTOGO_URL'])
+  uri = URI.parse(ENV.fetch("REDISTOGO_URL"))
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   $redis = Redis.new(host: "localhost", port: 6379)
 end
